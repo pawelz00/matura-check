153c0e281c44836bdaa53a7cb479f87484fcf93d
pawelz00 <pawet.wets@gmail.com>
2025-02-18 12:55:06 +0100

    feat: card group

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -2,6 +2,7 @@ import { useDataStore } from "@/store/useDataStore";
 import SingleCard from "@/components/card/single-card.tsx";
 import { useFiltersStore } from "@/store/useFiltersStore";
 import { cn } from "@/lib/utils.ts";
+import { CardGroup } from "@/components/card/card-group.tsx";
 
 export default function Cards() {
   const { data, groupedData, groupBy, statuses } = useDataStore();
@@ -33,30 +34,18 @@ export default function Cards() {
     <main className={"flex flex-col gap-12 w-1/2 mt-12 items-center"}>
       {Object.entries(groupedData ?? {}).map(([key, value]) => {
         return (
-          <section
-            key={key}
-            className={"flex flex-col shadow-md rounded-xl w-full bg-white"}
-          >
-            <span
-              className={
-                "text-2xl tracking-wide px-4 py-2 bg-black rounded-t-xl text-white"
-              }
-            >
-              {key}
-            </span>
-            <div className={cn(classNames[view], "px-8 py-8")}>
-              {value
-                .filter((item) => {
-                  if (status) {
-                    return statuses[item.id] === status;
-                  }
-                  return true;
-                })
-                .map((item) => (
-                  <SingleCard key={item.id} item={item} />
-                ))}
-            </div>
-          </section>
+          <CardGroup title={key} description={""}>
+            {value
+              .filter((item) => {
+                if (status) {
+                  return statuses[item.id] === status;
+                }
+                return true;
+              })
+              .map((item) => (
+                <SingleCard key={item.id} item={item} />
+              ))}
+          </CardGroup>
         );
       })}
     </main>
